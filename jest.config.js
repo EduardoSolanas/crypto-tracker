21f6967a3fb4f9f021fdc5778e53b89e3a8adba1
eduardo solanas <eduardo.solanas@gmail.com>
2026-03-13 01:31:21 +0000

    trying to fix the pipeline

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,8 @@ module.exports = {
     moduleNameMapper: {
         'react-native-wagmi-charts': '<rootDir>/src/__mocks__/react-native-wagmi-charts',
         'react-native-chart-kit': '<rootDir>/src/__mocks__/react-native-chart-kit',
+        '^react-native/Libraries/NativeComponent/ViewConfigIgnore(\\.js)?$': '<rootDir>/src/__mocks__/ViewConfigIgnore.js',
+        '^\\./ViewConfigIgnore(\\.js)?$': '<rootDir>/src/__mocks__/ViewConfigIgnore.js',
     },
     transformIgnorePatterns: [
         'node_modules/(?!((jest-)?react-native|@react-native|expo(nent)?|@expo(nent)?/.*|@react-navigation/.*|react-native-svg|react-native-gesture-handler|react-native-reanimated))',
--- src/__mocks__/ViewConfigIgnore.js
+++ src/__mocks__/ViewConfigIgnore.js
@@ -1,4 +1,10 @@
-module.exports = {
+const api = {
     ConditionallyIgnoredEventHandlers: (value) => value,
     DynamicallyInjectedByGestureHandler: (value) => value,
+};
+
+module.exports = {
+    __esModule: true,
+    ...api,
+    default: api,
 };
